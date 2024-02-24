112c24b96ba5fe2ccb784c391cad5ae527a5e961
seung <ksgg1navercom@gmail.com>
2024-02-24 23:40:09 +0900

    [Kotlin]<chore> : change font

--- src/main/kotlin/com/seunggil/auto_cost_settlement/service/PdfService.kt
+++ src/main/kotlin/com/seunggil/auto_cost_settlement/service/PdfService.kt
@@ -6,6 +6,7 @@ import com.itextpdf.html2pdf.HtmlConverter
 import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider
 import com.itextpdf.io.font.FontProgramFactory
 import com.itextpdf.kernel.*
+import com.itextpdf.kernel.geom.PageSize
 import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.layout.Document
@@ -21,7 +22,7 @@ class PdfService {
     // reference : https://mchch.tistory.com/116
     @Throws(IOException::class)
     fun renderHtmlToPdf(htmlContent: String, outputFilePath: String): Boolean {
-        val font = "src/main/resources/fonts/NanumSquareRoundB.ttf"
+        val font = "src/main/resources/fonts/NanumSquareRoundL.ttf"
 
         //ConverterProperties : htmlconverter의 property를 지정하는 메소드인듯
         val properties: ConverterProperties = ConverterProperties()
@@ -32,11 +33,11 @@ class PdfService {
 
         val elements: List<IElement> = HtmlConverter.convertToElements(htmlContent, properties)
         val pdf: PdfDocument = PdfDocument(PdfWriter(outputFilePath))
-        val document: Document = Document(pdf)
+        val document: Document = Document(pdf, PageSize.A4)
 
         //setMargins 매개변수순서 : 상, 우, 하, 좌
 
-        document.setMargins(0f, 30f, 0f, 30f)
+        document.setMargins(-30f, 0f, 0f, 50f)
         for (element in elements) {
             document.add(element as IBlockElement)
         }
