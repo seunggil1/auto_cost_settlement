dbf164226b58727c24ed1de8b483f40df5790946
seung <ksgg1navercom@gmail.com>
2024-02-25 00:35:15 +0900

    [Kotlin]<feat> : add BaeminService

--- src/main/kotlin/com/seunggil/auto_cost_settlement/Scheduler/EmailScheduler.kt
+++ src/main/kotlin/com/seunggil/auto_cost_settlement/Scheduler/EmailScheduler.kt
@@ -1,29 +1,36 @@
-package com.seunggil.auto_cost_settlement.service
+package com.seunggil.auto_cost_settlement.Scheduler
 
 
+import com.seunggil.auto_cost_settlement.service.BaeminService
+import com.seunggil.auto_cost_settlement.service.EmailService
+import com.seunggil.auto_cost_settlement.service.HtmlService
+import com.seunggil.auto_cost_settlement.service.PdfService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
-import java.nio.file.Paths
 
 
 @Service
 class EmailScheduler(
     private val emailService: EmailService,
     private val htmlService: HtmlService,
-    private val pdfService: PdfService
+    private val pdfService: PdfService,
+    private val baeminService: BaeminService
 
 ) {
 
     @Scheduled(fixedRate = 5000)
     fun onEmailReceived() {
         val host = "imap.naver.com"
-        val user = "{naverid}"
-        val password = "{naverpassword}"
+        val user = "{naverId}"
+        val password = "{naverPw}"
 
         val results = emailService.readEmails(host, user, password)
         for (result in results) {
             val link = htmlService.findHyperlinks(result)
             var content = htmlService.fetchContentFromUrl(link[0])
+            baeminService.extractMoney(content)
+            baeminService.extractDate(content)
+
             content = htmlService.convertCssLink(content)
 
             content = htmlService.removeScriptTag(content)
---
+++ src/main/kotlin/com/seunggil/auto_cost_settlement/service/BaeminService.kt
@@ -0,0 +1,52 @@
+package com.seunggil.auto_cost_settlement.service
+
+import org.springframework.stereotype.Service
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+@Service
+class BaeminService {
+    fun extractMoney(htmlContent : String) : Int {
+        val doc = Jsoup.parse(htmlContent)
+        val totalRows = doc.select("tr").filter { it -> it.text().contains("합계") }
+        var totalSum = 0
+
+        totalRows.forEach { row ->
+            val totalAmountStringBuilder = StringBuilder()
+            row.select("td").forEach { td ->
+                td.text().trim().takeIf { it.isNotEmpty() }?.let { number ->
+                    totalAmountStringBuilder.append(number)
+                }
+            }
+            totalSum += totalAmountStringBuilder.toString().toIntOrNull() ?: 0
+        }
+
+        return totalSum
+    }
+
+    fun extractDate(htmlContent: String) : LocalDateTime? {
+        val doc = Jsoup.parse(htmlContent)
+        val rows = doc.select("table.tb_type1 tr")
+
+        for (row in rows) {
+            // "결제일시"가 포함된 행을 찾습니다.
+            if (row.text().contains("결제일시")) {
+                val nextRow = row.nextElementSibling() // "결제일시"가 포함된 행의 다음 행
+                nextRow?.let {
+                    val dateTimeString = it.select("td").first()?.text() // 결제일시 값을 가져옵니다.
+                    dateTimeString?.let { dateTime ->
+                        // 날짜 및 시간을 LocalDateTime 형태로 파싱합니다.
+                        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                        return LocalDateTime.parse(dateTime, formatter)
+                    }
+                }
+                break
+            }
+        }
+        return null
+    }
+}
